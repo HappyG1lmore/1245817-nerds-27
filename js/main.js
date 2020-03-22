81e38f8583f961487b4646df8c113901be411cf7
G1lmore <cereperec@gmail.com>
2020-03-22 17:03:46 +0300

    final 3

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
-$(function () {
-  $('.slider').slick ({
 
-  });
+
+  var writeUs = document.querySelector ('.write-us-button');
+  var modal = document.querySelector ('.modal-write-us');
+  var close = document.querySelector ('.button-write-us-close');
+  var form = modal.querySelector ('form');
+
+  writeUs.addEventListener ('click', function (evt) {
+  evt.preventDefault();
+  modal.classList.add('modal-show');
 });
+  close.addEventListener ('click', function (evt) {
+  evt.preventDefault();
+  modal.classList.remove ('modal-show');
+});
+  form.addEventListener ('submit', function (evt) {
+  evt.preventDefault();
+  сonsole.log('Форма работает');
+});
+
+
+
+
