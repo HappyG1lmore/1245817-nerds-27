fb18f0586489cbee851f7da9d63a0f60afd0268b
G1lmore <cereperec@gmail.com>
2020-03-25 21:08:15 +0300

    Final 5

--- js/main.js
+++ js/main.js
@@ -1,13 +1,37 @@
-var writeUs = document.querySelector( '.write-us-button' );
-var modal = document.querySelector( '.modal-write-us' );
-var close = document.querySelector( '.button-write-us-close' );
-var form = modal.querySelector( 'form' );
+var writeUs = document.querySelector( ".write-us-button" );
+var modal = document.querySelector( ".modal-write-us" );
+var close = document.querySelector( ".button-write-us-close" );
+var form = modal.querySelector( "form" );
+var name = modal.querySelector( "[name=name1]" );
+var email = modal.querySelector( "[name=email]" );
 
-writeUs.addEventListener( 'click', function( evt ) {
+writeUs.addEventListener( "click", function( evt ) {
   evt.preventDefault();
-  modal.classList.add( 'modal-show' );
+  modal.classList.add( "modal-show" );
 } );
-close.addEventListener( 'click', function( evt ) {
+
+form.addEventListener( "submit", function( evt ) {
+  if ( !name.value || !email.value ) {
+    evt.preventDefault();
+    modal.classList.remove( "modal-error" );
+    modal.offsetWidth = modal.offsetWidth;
+    modal.classList.add( "modal-error" );
+  }
+} );
+
+close.addEventListener( "click", function( evt ) {
   evt.preventDefault();
-  modal.classList.remove( 'modal-show' );
+  modal.classList.remove( "modal-show" );
+  modal.classList.remove( "modal-error" );
+} );
+
+window.addEventListener( "keydown", function( evt ) {
+  if ( evt.keyCode === 27 ) {
+    evt.preventDefault();
+
+    if ( modal.classList.contains( "modal-show" ) ) {
+      modal.classList.remove( "modal-show" );
+      modal.classList.remove( "modal-show" );
+    }
+  }
 } );
